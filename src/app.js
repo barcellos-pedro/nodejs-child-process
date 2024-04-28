3e2cc1a66d94c134cdf1c4366976d504c7ba22d6
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2024-04-27 21:10:33 -0300

    Add database props and refactor

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
 import { seedDatabase, getPagedData } from "./database.js"
 import { initCluster } from "./cluster.js"
 
-seedDatabase()
+const databaseProps = {
+  seedCount: 60,
+  limit: 15,
+}
+
+seedDatabase(databaseProps.seedCount)
 
 const clusterProps = {
   maxWorkers: 2,
   script: "src/task.js",
   onTaskDone(count) {
-    console.log(`tasks done: ${count}`)
+    if (count == databaseProps.seedCount) {
+      console.log("All tasks done")
+      killAll()
+    }
   },
 }
 
 const { getWorker, killAll } = initCluster(clusterProps)
 
-for await (const data of getPagedData()) {
+for await (const data of getPagedData(databaseProps.limit)) {
   const worker = getWorker()
   worker.send(data)
 }
