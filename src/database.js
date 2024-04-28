735c50a806e88750421f5aa9246aed4a29d5fa4a
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2024-04-27 21:11:55 -0300

    Add param to seed and refactor getPagedData

--- src/database.js
+++ src/database.js
@@ -3,8 +3,8 @@ import { sleep } from "./helpers.js"
 
 const DB = []
 
-export function seedDatabase() {
-  for (let i = 1; i <= 30; i++) {
+export function seedDatabase(count = 60) {
+  for (let i = 1; i <= count; i++) {
     DB.push({
       id: i,
       name: faker.person.fullName(),
@@ -13,9 +13,13 @@ export function seedDatabase() {
   }
 }
 
-export async function* getPagedData(limit = 15) {
-  await sleep()
-  yield DB.slice(0, limit)
-  await sleep()
-  yield DB.slice(limit)
+export async function* getPagedData(limit = 10) {
+  do {
+    console.log("Items remaining:", DB.length)
+    await sleep()
+    yield DB.splice(0, limit)
+  } while (DB.length)
+
+  console.log("No items left:", DB.length)
+  return
 }
